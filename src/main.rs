e3ba43d835dcf1d8e1e06423b4245cf435534f80
absenth <1115138+absenth@users.noreply.github.com>
2023-12-09 09:18:05 -0600

    Reqwest can make reqwests on my local area network

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let qsos = reqwest::Client::new()
+        .get("http://bartmoss.uid0.online:5001/api/qso")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("{:#?}", qsos);
+
+       Ok(()) 
 }
